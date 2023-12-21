e3a2aa4229674eb18551fbcaec6a097a7eabeb50
Meliani15 <145130009+Meliani15@users.noreply.github.com>
2023-12-21 16:28:33 +0700

    Upload file latihan 1

---
+++ 231031095-meliani-latihan1.py
@@ -0,0 +1,13 @@
+#Percabangan if
+nilai = 78
+batas_lulus = 67
+if nilai > batas_lulus:
+ print('Selamat, kamu lulus!')
+
+#Percabangan else
+ nilai = 45
+batas_lulus = 67
+if nilai > batas_lulus:
+ print('Selamat, kamu lulus!')
+else:
+ print('Sayang sekali, kamu tidak lulus!')
