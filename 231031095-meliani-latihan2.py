0218b932f4a65c7dc92aee17c41ffc3360d71646
Meliani15 <145130009+Meliani15@users.noreply.github.com>
2023-12-21 16:29:17 +0700

    Upload file latihan 2

---
+++ 231031095-meliani-latihan2.py
@@ -0,0 +1,17 @@
+#ppercabangan if
+inp = input('Masukkan suatu input: ')
+if len(inp) != 3:
+ print('Panjang string ', len(inp),'tidak sama dengan 3')
+else:
+ print('Panjang input sesuai yang diinginkan')
+
+ #percabangan else
+ nilai = int(input('Masukkan nilai: '))
+if nilai >= 95:
+ print('Nilai huruf: A')
+elif nilai > 80:
+ print('Nilai huruf: B')
+elif nilai > 60:
+ print('Nilai huruf: C')
+else:
+ print('Nilai huruf: D')
