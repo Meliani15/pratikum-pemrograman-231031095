aa20e642116b0eab01cefa9d81779a0d828c0e72
Meliani15 <145130009+Meliani15@users.noreply.github.com>
2023-12-21 16:29:46 +0700

    Upload file latihan 3

---
+++ 231031095-meliani-latihan3.py
@@ -0,0 +1,12 @@
+#nomor tiga
+
+nama = input("Nama karyawan: ")
+pendapatan = int(input("Pendapatan: "))
+
+print("Nama karyawan:", nama) 
+print("Pendapatan:", pendapatan)
+
+if pendapatan > 8000:
+  print("Status: Berhak")
+else:
+  print("Status: Tidak Berhak")
